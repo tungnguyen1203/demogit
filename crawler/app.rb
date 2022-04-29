6416144c5a549457cf8ede67d5c81aa286bbe7a3
tungnguyen1203 <tt.tung.nguyen3@tomosia.com>
2022-04-29 17:28:13 +0700

    hoan thanh giai doan 1

--- crawler/app.rb
+++ crawler/app.rb
@@ -9,13 +9,19 @@ require './config/capybara'
 require './operation/crawl/helper_crawl'
 require './operation/crawl/site_generatedata_crawl'
 require './operation/generate/table_html'
+# require 'sinatra'
+
+
 # Crawl data
 crawl = Operation::Crawl::SiteGeneratedataCrawl.new
 crawl.start do
     # Generate TABLE HTML
-    html_saved_at = './publics/user_datatable.html'
-
+    html_saved_at = './publics/user_management.html'
     generate = Operation::Generate::TableHtml.new(crawl.read_file, saved_at: html_saved_at)
     generate.generate
     p "Generate HTML: #{html_saved_at}"
 end
+
+# get '/' do
+#     'Hello world!'
+# end
--- crawler/operation/generate/table_html.rb
+++ crawler/operation/generate/table_html.rb
@@ -2,8 +2,20 @@
 module Operation
   module Generate
     class TableHtml
-      def initialize(read_file, save_html)
-        binding.pry
+      def initialize(read_file, opts={})
+        @read_file = read_file
+        @opts = opts
+      end
+
+      def generate
+        erb_file = './templates/table.html.erb'
+        erb_str = File.read(erb_file)
+        renderer = ERB.new(erb_str)
+        result = renderer.result(binding)
+        File.open(@opts[:saved_at], 'w') do |f|
+          f.write(result)
+        end
+
       end
     end
   end
